b5611c31f1db7107b37571dc1fffd2cb8ba27eac
lafabigdatamg <bigdata.lafamg@outlook.com>
2022-03-24 15:44:21 -0300

    Alteração nome cluster eks.sh

--- infra/eks.sh
+++ infra/eks.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-eksctl create cluster --name=igtik8s --managed --spot --instance-types=m5.xlarge \
+eksctl create cluster --name=igtik8sv2 --managed --spot --instance-types=m5.xlarge \
     --nodes=2 --alb-ingress-access --node-private-networking --region=us-east-2 \
-    --nodes-min=2 --nodes-max=3 --full-ecr-access --asg-access --nodegroup-name=ng-igtik8s
+    --nodes-min=2 --nodes-max=3 --full-ecr-access --asg-access --nodegroup-name=ng-igtik8sv2
